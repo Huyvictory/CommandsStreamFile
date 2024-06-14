c25d2ef673cb99316ff89e4acad44fb853224040
Huyvictory <Huyvictory@HUYVICTORY-PC>
2024-06-14 22:27:27 +0700

    Add project files.

---
+++ CopyCommand/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace CopyCommand
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = @"C:\Users\vomin\OneDrive\HuyvictoryPC\.NET Basics\test.txt";
+            var target = @"C:\Users\vomin\OneDrive\HuyvictoryPC\.NET Basics\test-copy.txt";
+
+            // buffer memory for file copy operation
+            var bufferMemory = new byte[1024];
+            var inputStreamOpenFile = File.OpenRead(source);
+            var outputStreamWriteFile = File.OpenWrite(target);
+
+            // Read file and push into buffer memory
+            int n = inputStreamOpenFile.Read(bufferMemory, 0, bufferMemory.Length);
+
+            // File is not read completely
+            while (n > 0)
+            {
+                Console.WriteLine(n.ToString());
+
+                outputStreamWriteFile.Write(bufferMemory, 0, n);
+
+                n = inputStreamOpenFile.Read(bufferMemory, 0, bufferMemory.Length);
+            }
+
+            inputStreamOpenFile.Close();
+            outputStreamWriteFile.Close();
+        }
+    }
+}
---
+++ DirComand/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+
+namespace DirComand
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "C:\\Users\\vomin\\OneDrive\\Hình ảnh\\Ảnh chụp màn hình";
+
+            var dir = new DirectoryInfo(path);
+
+            var directories = dir.GetDirectories();
+
+            foreach (var directory in directories)
+            {
+                Console.WriteLine($"{directory.LastWriteTime.ToString("MM/dd/yyyy")} {directory.LastWriteTime.ToString("HH:mm")} <DIR> {directory.Name}");
+            }
+
+            var files = dir.GetFiles();
+
+            foreach (var file in files)
+            {
+                Console.WriteLine($"{file.LastWriteTime.ToString("MM/dd/yyyy")} {file.LastWriteTime.ToString("HH:mm")}  {file.Length.ToString("#,###")}      {file.Name}");
+            }
+        }
+    }
+}
